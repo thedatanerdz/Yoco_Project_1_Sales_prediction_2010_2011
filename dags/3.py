6441f2b782691f569cbee0db939026871cd8a6d1
Soleil Botha <122761529+thedatanerdz@users.noreply.github.com>
2023-12-18 18:55:58 +0200

    Delete dags/3.py

--- dags/3.py
+++
@@ -1,5 +0,0 @@
-@task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
-    def check_load(scan_name='check_load', checks_subpath='sources'):
-        from include.soda.check_function import check
-
-        return check(scan_name, checks_subpath)
