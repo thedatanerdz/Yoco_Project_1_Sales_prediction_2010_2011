a3dcd5d0dff9e27e2c8a9ade57660bae1a576769
Soleil Botha <122761529+thedatanerdz@users.noreply.github.com>
2023-12-18 18:58:19 +0200

    Update retail.py

--- dags/retail.py
+++ dags/retail.py
@@ -3,16 +3,20 @@
 from airflow.decorators import dag, task
 from datetime import datetime
 
-from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from airflow.providers.google.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.operators.bigquery import BigQueryCreateEmptyDatasetOperator
 from astro import sql as aql
 from astro.files import File
 from astro.sql.table import Table, Metadata
 from astro.constants import FileType
+from include.dbt.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
+from cosmos.airflow.task_group import DbtTaskGroup
+from cosmos.constants import LoadMode
+from cosmos.config import ProjectConfig, RenderConfig
 
 @dag(
     start_date=datetime(2023, 1, 1),
-    schedule=None,
+    schedule_interval=None,
     catchup=False,
     tags=['retail'],
 )
@@ -23,14 +27,14 @@ def retail():
         src='include/dataset/online_retail.csv',
         dst='raw/online_retail.csv',
         bucket='soleil_online_retail',
-        gcp_conn_id='gcp',
+        google_cloud_storage_conn_id='gcp',
         mime_type='text/csv',
     )
 
     create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
         task_id='create_retail_dataset',
         dataset_id='retail',
-        gcp_conn_id='gcp',
+        google_cloud_storage_conn_id='gcp',
     )
 
     gcs_to_raw = aql.load_file(
@@ -48,8 +52,41 @@ def retail():
         use_native_support=False,
     )
 
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_load(scan_name='check_load', checks_subpath='sources'):
+        from include.soda.check_function import check
+        return check(scan_name, checks_subpath)
+
+    transform = DbtTaskGroup(
+        group_id='transform',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/transform']
+        )
+    )
+
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_transform(scan_name='check_transform', checks_subpath='transform'):
+        from include.soda.check_function import check
+        return check(scan_name, checks_subpath)
+
+    report = DbtTaskGroup(
+        group_id='report',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/report']
+        )
+    )
+
     # Set the task dependencies
     upload_csv_to_gcs >> create_retail_dataset >> gcs_to_raw
+    gcs_to_raw >> check_load
+    create_retail_dataset >> transform >> check_transform
+    gcs_to_raw >> report
 
 # Instantiate the DAG to be discovered by Airflow
 retail_dag = retail()
